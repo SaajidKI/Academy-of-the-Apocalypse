42fa48f48e6ca2f9f7195c4820dbb94535cc0cf0
banban224x <banban224x@gmail.com>
2023-05-09 17:32:45 -0400

    camera doesnt shake in pause

--- Academy of the Apocalypse/Assets/Scripts/Sushi Ninja Scripts/CameraShake.cs
+++ Academy of the Apocalypse/Assets/Scripts/Sushi Ninja Scripts/CameraShake.cs
@@ -6,15 +6,26 @@ public class CameraShake : MonoBehaviour{
 
        public float durationTime = 0.15f;
        public float magnitude = 0.3f;
+       public bool enable = true;
 
        //The Update Function is just for testing (hit [p]), and can be commented out:
        void Update(){
-              
+
        }
 
        //use this to call from another script, like when the player gets hit
        public void ShakeCamera(float durationTime2, float magnitude2){
-              StartCoroutine(ShakeMe(durationTime2, magnitude2));
+              if (enable == true) {
+                     StartCoroutine(ShakeMe(durationTime2, magnitude2));
+              }
+       }
+
+       public void disableShake() {
+              enable = false;
+       }
+
+       public void enableShake() {
+              enable = true;
        }
 
        //the screenshake!
